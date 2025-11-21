e3b8970babe0809f6114cee05a80be41543ab0ef
Vaishvi07 <91317372+Vaishvi07@users.noreply.github.com>
2025-11-21 17:38:05 +0530

    reconnectivity implementation

--- lib/screens/connectivity_error_screen.dart
+++ lib/screens/connectivity_error_screen.dart
@@ -1,9 +1,26 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-class ConnectivityErrorScreen extends StatelessWidget {
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:flutter/material.dart';
+import 'package:project_management_user/screens/project_details_screen.dart';
+import 'package:project_management_user/screens/splash_screen.dart';
+
+import '../shared_preferences/shared_pref.dart';
+import 'login_screen.dart';
+
+class ConnectivityErrorScreen extends StatefulWidget {
   final VoidCallback? onRetry;
 
-  const ConnectivityErrorScreen({super.key, this.onRetry});
+  ConnectivityErrorScreen({super.key, this.onRetry});
+
+  @override
+  State<ConnectivityErrorScreen> createState() => _ConnectivityErrorScreenState();
+}
+
+class _ConnectivityErrorScreenState extends State<ConnectivityErrorScreen> {
+  StreamSubscription<List<ConnectivityResult>>? _connectivitySubscription;
+
+  bool _navigated = false;
 
   @override
   Widget build(BuildContext context) {
@@ -70,6 +87,18 @@ class ConnectivityErrorScreen extends StatelessWidget {
                   ),
                 ),
               ),
+              TextButton(
+                onPressed: () {
+                  _checkConnectivity();
+                },
+                child: Text(
+                  "Try Again",
+                  style: TextStyle(
+                    color: Colors.blue.shade600,
+                    fontSize: 14,
+                  ),
+                ),
+              ),
             ],
           ),
         ),
@@ -96,5 +125,41 @@ class ConnectivityErrorScreen extends StatelessWidget {
         ],
       ),
     );
+  }
+
+  void _checkConnectivity() {
+    _connectivitySubscription =
+        Connectivity().onConnectivityChanged.listen((List<ConnectivityResult> result) async {
+          if (_navigated) return; // prevent double navigation
+
+          if (result.contains(ConnectivityResult.mobile) ||
+              result.contains(ConnectivityResult.wifi)) {
+            bool isLoggedIn = await SharedPref.getLoginStatus();
+
+            setState(() {
+              _navigated = true;
+            });
+
+            if (isLoggedIn) {
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(builder: (context) => const ProjectDetailsScreen()),
+              );
+            } else {
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(builder: (context) => const LoginScreen()),
+              );
+            }
+          } else {
+            setState(() {
+              _navigated = true;
+            });
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(builder: (context) =>  ConnectivityErrorScreen()),
+            );
+          }
+        });
   }
 }
